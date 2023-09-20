ba2a74a23c833eb30df267b29513f430bc454d04
dean06001 <dean06001@E-PW0234KC>
2023-09-20 09:59:58 +0200

    Add project files.

---
+++ uppgift 2.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+    namespace uppgift_2_1
+    {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilken är din favorit bok?");
+            string bok = Console.ReadLine();
+            Console.WriteLine("din favorite bok är " + bok +".");
+            Console.ReadKey();
+        }
+    }
+}
